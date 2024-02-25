6c7e696ba0862d2706ae18fa4b195e1780501a56
AbdulhakimKaya <abdulhakimkaya2199@gmail.com>
2024-02-25 21:56:05 +0300

    Lesson 3 Commit 2

--- Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07dd0d1bed9d87b3056944b9c169aafacf2fcb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2b37dca008ad038283242034e92e55b366ac1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
